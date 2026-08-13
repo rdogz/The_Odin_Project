17c73646cee7507fda25dbff17bd77cfd0fa0e6e
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-13 15:10:41 -0300

    javascript-exercises/foundations/09: Done

--- Foundations/javascript/javascript-exercises/foundations/09_sumAll/sumAll.js
+++ Foundations/javascript/javascript-exercises/foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if (!(Number.isInteger(start)) || !(Number.isInteger(end))) {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+    if (end < start) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+
+    let result = 0;
+
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
