b6ee775a1c1c751ccbd946ba50883479ac31dae7
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-13 14:42:27 -0300

    javascript-exercises/foundations/08: Done

--- Foundations/javascript/javascript-exercises/foundations/08_removeFromArray/removeFromArray.js
+++ Foundations/javascript/javascript-exercises/foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    let newArray = arr;
+    for (let i = 0; i < toRemove.length; i++) {
+        newArray = newArray.filter((itemAtIndex) => itemAtIndex !== toRemove[i]);
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
