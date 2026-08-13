3bc6ace4c68719cab598be45850c880f96796f40
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-13 14:08:19 -0300

    javascript-exercises/foundations/06: Done

--- Foundations/javascript/javascript-exercises/foundations/06_repeatString/repeatString.js
+++ Foundations/javascript/javascript-exercises/foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else if (num == 0) {
+        return "";
+    }
 
+    let newString = string;
+
+    for (let i = 0; i < num - 1; i++) {
+        newString = newString + string;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
