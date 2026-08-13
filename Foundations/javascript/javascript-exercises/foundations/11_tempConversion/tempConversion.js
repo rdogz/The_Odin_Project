c6a38ca21c3a59b13efaeded6846c0fd10a18bbd
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-13 15:52:12 -0300

    javascript-exercises/foundations/11: Done

--- Foundations/javascript/javascript-exercises/foundations/11_tempConversion/tempConversion.js
+++ Foundations/javascript/javascript-exercises/foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahTemp) {
+    let conversion = (fahTemp - 32) / 1.8; 
+    if (Number.isInteger(conversion)) {
+        return conversion;
+    }
+
+    return Math.round(conversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+    let conversion = celTemp * 1.8 + 32; 
+    if (Number.isInteger(conversion)) {
+        return conversion;
+    }
+
+    return Math.round(conversion * 10) / 10;
 };
 
 // Do not edit below this line
