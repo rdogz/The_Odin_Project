263d69d327868866dd09028f7799b344737f8f90
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-13 14:16:54 -0300

    javascript-exercises/foundations/07: Done

--- Foundations/javascript/javascript-exercises/foundations/07_reverseString/reverseString.js
+++ Foundations/javascript/javascript-exercises/foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    newString = "";
 
+    let inputStringLength = inputString.length;
+    for (let i = 0; i <= inputStringLength; i++) {
+        newString = newString + inputString.charAt(inputStringLength - i);
+    }
+
+    console.log(newString);
+    return newString;
 };
 
 // Do not edit below this line
