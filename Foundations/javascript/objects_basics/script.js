018c6a352e01b4009ddb1ecc6d00df1bc888ca2d
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-15 18:05:31 -0300

    foundations-object-basics

---
+++ Foundations/javascript/objects_basics/script.js
@@ -0,0 +1,13 @@
+const person = {
+    name: {
+        first: "Bob", 
+        last: "Smith",
+    },
+    age: 32,
+    bio() {
+        console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+    },
+    introduceSelf() {
+        console.log(`Hi! I'm ${this.name[0]}.`);
+    },
+};
