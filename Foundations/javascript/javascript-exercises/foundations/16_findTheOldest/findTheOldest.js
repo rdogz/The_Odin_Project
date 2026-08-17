5ec374c67c7a46c8cb2a8289eea5bdb513e4eeca
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-17 12:49:58 -0300

    javascript exercises 16_findTheOldest: Done

--- Foundations/javascript/javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
+++ Foundations/javascript/javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sorted = people.sort((a, b) => {
+        const aDeath = a.yearOfDeath ?? new Date().getFullYear();
+        const bDeath = b.yearOfDeath ?? new Date().getFullYear();
 
+        return (aDeath - a.yearOfBirth) - (bDeath - b.yearOfBirth);
+    });
+    return sorted[sorted.length - 1];
 };
 
 // Do not edit below this line
