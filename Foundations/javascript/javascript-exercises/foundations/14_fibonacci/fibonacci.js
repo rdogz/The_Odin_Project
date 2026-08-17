11e3cc8fd4da6fe11bf0dd75eee1f2084b5956fa
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-17 12:14:13 -0300

    javascript exercises 14_fibonacci: Done

--- Foundations/javascript/javascript-exercises/foundations/14_fibonacci/fibonacci.js
+++ Foundations/javascript/javascript-exercises/foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(target) {
+    if (target < 0) {
+        return "OOPS";
+    }
 
+    if (target == 0) {
+        return 0;
+    }
+
+    let a = 1;
+    let b = 1;
+    let c = 1;
+
+    for (let i = 2; i < target; i++) {
+        c = a + b;
+        b = a;
+        a = c;
+    }
+
+    return c;
 };
 
 // Do not edit below this line
