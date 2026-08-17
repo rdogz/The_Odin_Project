22723de71032dfd750d91c14547c1a486e6ec1d9
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-17 11:48:33 -0300

    javascript exercises 13_palindromes: Done

--- Foundations/javascript/javascript-exercises/foundations/13_palindromes/palindromes.js
+++ Foundations/javascript/javascript-exercises/foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const inputStr = str.toLowerCase().replace(/[^\p{L}\p{N}]/gu, "");
 
+    return inputStr === inputStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
