0dd035d2f09c87c84b4d09d4316c0166d2f041c9
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-18 14:24:01 -0300

    PROJECT CALCULATOR: Done
     Calculator is still a little buggy, but attends project requirements.
    
     To fix:
    	Really big multiplications (javascript issue but should render error);
    	CSS bugs for smaller displays.
    
     To add:
    	Negative number operations;
    	Keyboard support.

--- Foundations/javascript/PROJECT_calculator/script.js
+++ Foundations/javascript/PROJECT_calculator/script.js
@@ -5,51 +5,142 @@ const clearButton = document.querySelector("#clear");
 const deleteButton = document.querySelector("#delete");
 const numbersButton = document.querySelectorAll(".number");
 const operationButton = document.querySelectorAll(".operation");
+const equalsButton = document.querySelector("#equals");
 
 
-let a = 0;
-let b = 0;
+let a = "";
+let b = "";
 let operator = "";
 
 /* DOM MANIPULATION STUFF */
-clearButton.addEventListener("click", () => 
-    display.textContent = ""
+clearButton.addEventListener("click", () => {
+        display.textContent = "";
+        clearVars();
+    }
 );
 
 deleteButton.addEventListener("click", () => {
-    display.textContent = display.textContent.slice(0, -1);
+    if (b !== "") {
+        b = b.slice(0, -1);
+        if (b === ""){
+            renderOperator(operator);
+            return;
+        }
+        display.textContent = display.textContent.slice(0, -1);
+    } else if (operator !== "") {
+        operator = "";
+        updateDisplay(a);
+    } else if (a !== "") {
+        a = a.slice(0, -1);
+        display.textContent = display.textContent.slice(0, -1);
+    }
 });
 
 numbersButton.forEach(button => {
     button.addEventListener("click", () => {
-        a = button.value;
-        addToDisplay(a);
-        console.log(a);
+        const value = button.value;
+        if (operator === "") {
+            if (value === "." && a.includes(".")){
+                return;
+            }
+            if (a.length < 14) {
+                a = `${a}${value}`;
+                console.log(a);
+                updateDisplay(a);
+            } else {
+                alert("Can't enter more than 14 digits!");
+            }
+        } else {
+            if (value === "." && b.includes(".")){
+                return;
+            }
+            if (b.length < 14) {
+                b = `${b}${value}`;
+                console.log(b);
+                updateDisplay(b);
+            } else {
+                alert("Can't enter more than 14 digits!");
+            }        
+        }
     })
 })
 
 operationButton.forEach(button => {
     button.addEventListener("click", () => {
-        operator = button.value;
-        console.log(operator);
+        if (operator === "" && a != "") {
+            operator = button.value;
+            renderOperator(operator);
+        } else if (operator !== "" && a !== "" && b !== ""){
+            a = operate(operator, Number(a), Number(b));
+            operator = button.value;
+            b = "";
+            updateDisplay(a);
+        } else if (operator !== "" && a !== "" && b === "") {
+            operator = button.value;
+            renderOperator(operator);
+        }
     })
 })
 
+equalsButton.addEventListener("click", () => {
+    if (a !== "" && b !== "" && operator !== "") {
+        updateDisplay(operate(operator, Number(a), Number(b)));
+        clearVars();
+    } 
+})
 
+function updateDisplay(input) {
+    let inputToString = String(input);
 
+    if (inputToString.includes(".") && inputToString.length > 14) {
+        inputToString = inputToString.slice(0, 14);
+    }
+    if (inputToString.length > 14) {
+        display.textContent = "Error";
+        return;
+    }
 
-function operate(a, operator, b) {
-    // TODO
+    display.textContent = inputToString;
 }
 
+function clearVars() {
+    a = "";
+    b = "";
+    operator = "";
+}
 
-function addToDisplay(input) {
-    if (display.textContent.length < 14) {
-        display.textContent += input;
-    } else {
-        display.textContent = input;
+function renderOperator(operator) {
+    switch (operator) {
+        case "+":
+            updateDisplay("+");
+            break;
+        case "-":
+            updateDisplay("-");
+            break;
+        case "*":
+            updateDisplay("x");
+            break;
+        case "/":
+            updateDisplay("÷");
+            break;
     }
 }
+
+/* MATH STUFF */
+function operate(operator, a, b) {
+    // TODO
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
+
 
 /* OPERATION FUNCTIONS */
 function add(a, b) {
@@ -65,5 +156,10 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        return "NaN";
+    }
     return a / b;
 }
+
+
