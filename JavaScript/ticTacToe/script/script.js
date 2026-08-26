692e54356aaa9f718d73a9d3c7917a18252ee35c
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-26 16:01:15 -0300

    JavaScript: Tic Tac Toe project board obj

--- JavaScript/ticTacToe/script/script.js
+++ JavaScript/ticTacToe/script/script.js
@@ -1,1 +1,21 @@
 console.log("hello");
+
+const board = (() => {
+    const gameboard = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+
+    return { gameboard };
+})();
+
+board.gameboard[0][0] = "x";
+
+function Game(params) {
+    
+}
+
+function Player(params) {
+    
+}
