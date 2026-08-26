3f191e0c5ae5b8c51a25f5ab4dc100acdf735fe6
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-26 12:13:31 -0300

    Library project almost finished  Missing feature: Update the DOM when toggling book.read

--- JavaScript/library/script/script.js
+++ JavaScript/library/script/script.js
@@ -4,7 +4,25 @@
 
 const myLibrary = [];
 const domLibrary = document.querySelector(".library");
+const form = document.querySelector("form");
 
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formTitle = document.querySelector('[name="form-book-title"]').value;
+    const formAuthor = document.querySelector('[name="form-book-author"]').value;
+    const formPages = document.querySelector('[name="form-book-pages"]').value;
+    const formRead = document.querySelector('[name="form-book-read"]').checked;
+
+    console.log(formTitle);
+    console.log(formAuthor);
+    console.log(formPages);
+    console.log(formRead);
+
+    addBookToLibrary(formTitle, formAuthor, formPages, formRead);
+    updateDOM(myLibrary, myLibrary.length-1);
+});
 
 /* Book object */
 function Book() {
@@ -16,10 +34,18 @@ function Book() {
     this.title;
     this.author;
     this.pages; 
-    this.read; 
+    this.read = false; 
 
-    function info() {
+    Book.prototype.info = function() {
         return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`
+    }
+
+    Book.prototype.toggleRead = function(library, idToUpdate) {
+       const bookToUpdate = library.find(book => book.id === idToUpdate);
+
+        if (bookToUpdate) {
+            bookToUpdate.read = !bookToUpdate.read;
+        }
     }
 }
 
@@ -36,32 +62,52 @@ function addBookToLibrary(inputTitle, inputAuthor, inputPages, inputRead) {
     myLibrary.push(inputBook);
 }
 
-function updateDOM(library) {
-    for (let i = 0; i < library.length; i++) {
-        let newBook = document.createElement("div");
-        let newTitle = document.createElement("h1");
-        let newAuthor = document.createElement("p");
-        let newPages = document.createElement("p");
-        let newRead = document.createElement("p");
+function updateDOM(library, index) {
+    let newBook = document.createElement("div");
+    let newTitle = document.createElement("h1");
+    let newAuthor = document.createElement("p");
+    let newPages = document.createElement("p");
+    let newRead = document.createElement("button");
+    let newButton = document.createElement("button");
 
-        newTitle.innerText = library[i].title;
-        newAuthor.innerText = `Author: ${library[i].author}`;
-        newPages.innerText = `Pages: ${library[i].pages}`;
+    newButton.innerText = "X";
+    newTitle.innerText = library[index].title;
+    newAuthor.innerText = `Author: ${library[index].author}`;
+    newPages.innerText = `Pages: ${library[index].pages}`;
 
-        if (library[i].read) {
-            newRead.style.backgroundColor = "green"
-            newRead.innerText = "Book finished";
-        } else{
-            newRead.style.backgroundColor = "red"
-            newRead.innerText = "book not yet finished";
-        }
-
-        newBook.classList.add("book");
-
-        domLibrary.appendChild(newBook);
-        newBook.appendChild(newTitle);
-        newBook.appendChild(newAuthor);
-        newBook.appendChild(newPages);
-        newBook.appendChild(newRead);
+    if (library[index].read) {
+        newRead.style.backgroundColor = "green";
+        newRead.innerText = "Book finished";
+    } else {
+        newRead.style.backgroundColor = "red";
+        newRead.innerText = "Book not yet finished";
     }
+
+    newButton.addEventListener("click", (event) => {
+        const bookId = event.target.closest(".book").id;
+        const book = document.getElementById(bookId);
+
+        book.remove();
+    });
+
+
+    newRead.addEventListener("click", (event) => {
+        const bookId = event.target.closest(".book").id;
+        const book = library.find((Book) => Book.id == bookId);
+
+        if (book) {
+            book.toggleRead(library, bookId);
+        }
+    });
+
+    newBook.classList.add("book");
+    newBook.setAttribute("id", library[index].id);
+
+    newBook.appendChild(newButton);
+    newBook.appendChild(newTitle);
+    newBook.appendChild(newAuthor);
+    newBook.appendChild(newPages);
+    newBook.appendChild(newRead);
+
+    domLibrary.appendChild(newBook);
 }
