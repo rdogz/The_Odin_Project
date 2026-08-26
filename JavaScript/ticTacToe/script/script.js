6394417066ad7a2fa5c49cf7b861b611d2978406
rdogz <213889694+rdogz@users.noreply.github.com>
2026-08-26 19:52:27 -0300

    JavaScript: Tic Tac Toe project add reset function

--- JavaScript/ticTacToe/script/script.js
+++ JavaScript/ticTacToe/script/script.js
@@ -7,7 +7,15 @@ const board = (() => {
         ["", "", ""]
     ];
 
-    return { gameboard };
+    const reset = function () {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j ++) {
+                gameboard[i][j] = "";
+            }
+        }
+    }
+
+    return { gameboard, reset };
 })();
 
 board.gameboard[0][0] = "x";
