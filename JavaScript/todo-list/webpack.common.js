d86770abec01893ac4937de2f780987bc8cc4349
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-06 18:01:43 -0300

    Javascript: todo | npm css-loader stuff

--- JavaScript/todo-list/webpack.common.js
+++ JavaScript/todo-list/webpack.common.js
@@ -1,15 +1,20 @@
+// webpack.config.js
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
+  mode: "development",
   entry: "./src/index.js",
-
   output: {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
   },
-
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
@@ -18,11 +23,5 @@ export default {
       },
     ],
   },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
 };
 
