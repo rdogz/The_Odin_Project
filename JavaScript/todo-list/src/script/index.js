7169ea2bfa7316a85c643a4a57dc09baf4f596b9
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-08 20:59:12 -0300

    Javascript: Todo | simplify code, app working

--- JavaScript/todo-list/src/script/index.js
+++ JavaScript/todo-list/src/script/index.js
@@ -1,58 +1,84 @@
 // IMPORTS
-
 import "../css/style.css";
 import "../css/reset.css";
 import "../css/side-bar.css";
 import "../css/content.css";
 import "../css/forms.css";
 
+
 import { Task, Project, TodoApp } from "./appLogic.js";
 import { getTask, renderTask, getProject, renderProject } from "./ui.js";
 
-///////////////////////////////////////////////////////////////
 
-
-// TESTING STUFF
-const taskForm = document.querySelector("#task-form"); 
-const taskDialog = document.querySelector("#add-task"); 
+const taskForm = document.querySelector("#task-form");
+const taskDialog = document.querySelector("#add-task");
 const projectForm = document.querySelector("#project-form");
 const projectDialog = document.querySelector("#add-project");
+//////////////////////////////////////////////////////////
 
-const firstTask = new Task("Water plants", "water them bro", "2026-09-08", "low", false);
 
-const tasks = [firstTask];
-let currentProject = new Project("default", tasks);
-const projects = [currentProject];
+const firstTask = new Task(
+    "Water plants",
+    "water them bro",
+    "2026-09-08",
+    "low",
+    false
+);
+const defaultProject = new Project("Default", [firstTask]);
+const appControl = new TodoApp([defaultProject]);
 
-const appControl = new TodoApp(projects); 
+let currentProject = defaultProject;
 
-renderTask(currentProject.tasks[0], currentProject);
 
+appControl.projects.forEach(project => {
+    renderProject(project, selectProject);
+});
+renderTask(firstTask, currentProject);
+
+
+// Add task
 taskForm.addEventListener("submit", (event) => {
     event.preventDefault();
-
     const task = getTask();
 
     currentProject.addTask(task);
-
     renderTask(task, currentProject);
+
     taskForm.reset();
     taskDialog.close();
 });
 
+
+// Add project
 projectForm.addEventListener("submit", (event) => {
     event.preventDefault();
-
     const project = getProject();
 
     appControl.addProject(project);
-    currentProject = project;
+    renderProject(project, selectProject);
 
-    console.log(currentProject);
-    console.log(currentProject.name);
-    renderProject(currentProject);
     projectForm.reset();
     projectDialog.close();
 });
 
-/////////////////////////////////////////////////////////////
+
+function selectProject(project) {
+    currentProject = project;
+    renderCurrentProject();
+}
+
+
+function renderCurrentProject() {
+    const content = document.querySelector("#content");
+    const existingTasks = content.querySelectorAll(".todo-item");
+    const projectTitle = content.querySelector("#project-title");
+    projectTitle.innerText = `Project: ${currentProject.name}`;
+
+    existingTasks.forEach(task => {
+        task.remove();
+    });
+    
+    currentProject.tasks.forEach(task => {
+        renderTask(task, currentProject);
+    });
+}
--- JavaScript/todo-list/src/script/ui.js
+++ JavaScript/todo-list/src/script/ui.js
@@ -1,43 +1,52 @@
-import { Task, Project, TodoApp } from "./appLogic.js";
+import { Task, Project, TodoApp } from "./appLogic.js"; 
 
 const getTask = () => {
-        const taskName = document.querySelector('[name="task-name"]').value;
-        const taskDescription = document.querySelector('[name="task-description"]').value;
-        const taskDate = document.querySelector('[name="task-date"]').value;
-        const taskPriority = document.querySelector('[name="task-priority"]').value;
+    const taskName = document.querySelector('[name="task-name"]').value;
+    const taskDescription = document.querySelector('[name="task-description"]').value;
+    const taskDate = document.querySelector('[name="task-date"]').value;
+    const taskPriority = document.querySelector('[name="task-priority"]').value;
 
-        return new Task(
-            taskName,
-            taskDescription,
-            taskDate,
-            taskPriority,
-            false
-        )
+    return new Task(
+        taskName,
+        taskDescription,
+        taskDate,
+        taskPriority,
+        false
+    );
 };
 
-const renderTask = (currentTask, currentProject) => {
-    const project = document.querySelector("#content");
+const getProject = () => {
+    const projectName = document.querySelector('[name="project-name"]').value;
+
+    return new Project(projectName);
+};
+
+const renderTask = (task, project) => {
+    const content = document.querySelector("#content");
 
     const taskElement = document.createElement("div");
-    taskElement.id = currentTask.id;
     taskElement.classList.add("todo-item");
+    taskElement.dataset.taskId = task.id;
 
     const taskHeader = document.createElement("div");
     taskHeader.classList.add("todo-item-header");
 
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.name = "item-check";
-    checkbox.value = "true";
+    checkbox.checked = task.completed;
+
+    checkbox.addEventListener("change", () => {
+        task.changeComplete();
+    });
 
     const taskTitle = document.createElement("h2");
-    taskTitle.innerText = currentTask.title;
+    taskTitle.innerText = task.title;
 
     const deleteButton = document.createElement("button");
     deleteButton.innerText = "X";
 
     deleteButton.addEventListener("click", () => {
-        currentProject.removeTask(currentTask.id);
+        project.removeTask(task.id);
         taskElement.remove();
     });
 
@@ -46,41 +55,39 @@ const renderTask = (currentTask, currentProject) => {
     taskHeader.appendChild(deleteButton);
 
     const taskDescription = document.createElement("p");
-    taskDescription.innerText = currentTask.description;
+    taskDescription.innerText = task.description;
 
     const taskDate = document.createElement("p");
-    taskDate.innerText = `Due date: ${currentTask.dueDate}`;
+    taskDate.innerText = `Due date: ${task.dueDate}`;
 
     taskElement.appendChild(taskHeader);
     taskElement.appendChild(taskDescription);
     taskElement.appendChild(taskDate);
 
-    if (currentTask.priority === "high") {
+    if (task.priority === "high") {
         taskElement.style.borderColor = "orangered";
-    } else if (currentTask.priority === "medium") {
+    } else if (task.priority === "medium") {
         taskElement.style.borderColor = "yellow";
-    } else if (currentTask.priority === "low") {
+    } else if (task.priority === "low") {
         taskElement.style.borderColor = "lightblue";
     }
 
-    project.appendChild(taskElement);
+    content.appendChild(taskElement);
 };
 
+const renderProject = (project, onClick) => {
+    const container = document.querySelector(".sidebar-project-container");
 
-const getProject = () => {
-        const projectName = document.querySelector('[name="project-name"]').value;
+    const button = document.createElement("button");
 
-        return new Project(projectName);
-};
+    button.type = "button";
+    button.innerText = `# ${project.name}`;
 
-const renderProject = (currentProject) => {
-    const projectContainer = document.querySelector(".sidebar-project-container");
+    button.addEventListener("click", () => {
+        onClick(project);
+    });
 
-    const newProject = document.createElement("button");
-    newProject.innerText = `# ${currentProject.name}`
-
-
-    projectContainer.appendChild(newProject);
+    container.appendChild(button);
 };
 
 export { getTask, renderTask, getProject, renderProject };
