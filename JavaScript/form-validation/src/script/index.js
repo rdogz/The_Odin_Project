0a558b72b90cab9daeca7028cdb45c94a389bcdc
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-08 23:40:43 -0300

    Javascript: Form validation | start script

--- JavaScript/form-validation/src/script/index.js
+++ JavaScript/form-validation/src/script/index.js
@@ -1,2 +1,17 @@
 import "../css/style.css";
 import "../css/reset.css";
+
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const postal = document.getElementById("postal");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("password-confirm");
+const submitButton = document.querySelection("button");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
