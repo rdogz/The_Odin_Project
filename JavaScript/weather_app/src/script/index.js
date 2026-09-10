0a2d09fb25131776ad7699e6d4edf75292980513
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-10 17:11:36 -0300

    Javascript: Weather | clean data & render temp

--- JavaScript/weather_app/src/script/index.js
+++ JavaScript/weather_app/src/script/index.js
@@ -3,11 +3,17 @@ import "../css/reset.css";
 
 const API_KEY = "BS237RFUVGMVKEQTF9XZYBNMU";
 const button = document.querySelector("button");
+const form = document.querySelector("form");
+const currentTime = new Date().toLocaleTimeString();
+const city = document.querySelector("input");
+const weatherDiv = document.querySelector("#weather-display");
+
+console.log(`Current time: ${currentTime}`);
 
 async function getWeather(city) {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/next5days?unitGroup=us&key=${API_KEY}`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/next1days?unitGroup=us&key=${API_KEY}`,
     );
 
     if (!response.ok) {
@@ -22,16 +28,36 @@ async function getWeather(city) {
   }
 }
 
-const cleanData = (obj) => {};
+const cleanData = (obj) => {
+  return {
+    city: obj.resolvedAddress,
+    curTemp: obj.days[0].temp,
+    tomorrowTemp: obj.days[1].temp,
+    curSensation: obj.days[0].feelslike,
+    curHumidity: obj.days[0].humidity,
+    sunUp: obj.days[0].sunrise,
+    sunDown: obj.days[0].sunset,
+    weatherDescription: obj.description,
+  };
+};
 
-button.addEventListener("click", async () => {
-  const weather = await getWeather("New York");
+function renderWeather(obj) {
+  const tempP = document.createElement("p");
+  tempP.innerText = obj.curTemp;
 
-  console.log(weather);
+  weatherDiv.appendChild(tempP);
+}
 
-  const weatherDescription = weather.description;
-  const temperature = weather.days[0].tempmax;
-  //TODO
-  const sunUp = "weather.current";
-  console.log(temperature);
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  try {
+    const weather = await getWeather(city.value);
+    const data = cleanData(weather);
+
+    renderWeather(data);
+    console.log(data);
+  } catch {
+    throw new Error("HTTP error");
+  }
 });
