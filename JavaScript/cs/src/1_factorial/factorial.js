828ee0e881a67568ca2a485734d6663ae28e3f28
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-11 15:28:30 -0300

    Javascript: recursion 1 done

---
+++ JavaScript/cs/src/1_factorial/factorial.js
@@ -0,0 +1,14 @@
+const factorial = function (n) {
+  if (!Number.isInteger(n) || n < 0) {
+    return undefined;
+  }
+
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+
+// Do not edit below this line
+module.exports = factorial;
