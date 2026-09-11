975b8dff7d66778f61d5deba23d336cb78d6dddc
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-11 17:26:02 -0300

    Javascript: recursion 4 done

--- JavaScript/cs/src/4_permutations/permutations.js
+++ JavaScript/cs/src/4_permutations/permutations.js
@@ -1,6 +1,26 @@
-const permutations = function() {
-  
+const permutations = function (arr) {
+  return curPerm(arr);
 };
-  
+
+function curPerm(arr) {
+  if (arr.length === 0) {
+    return [[]];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+    const remaining = arr.slice(0, i).concat(arr.slice(i + 1));
+
+    const permutations = curPerm(remaining);
+
+    for (const permutation of permutations) {
+      result.push([current, ...permutation]);
+    }
+  }
+
+  return result;
+}
 // Do not edit below this line
 module.exports = permutations;
