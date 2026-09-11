4de613cc9550499ece4c5a812920faa3861c69a8
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-11 16:39:11 -0300

    Javascript: recursion 3 done

--- JavaScript/cs/src/3_totalIntegers/totalIntegers.js
+++ JavaScript/cs/src/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,30 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (thing) {
+  if (typeof thing === "object" || typeof thing === "array") {
+    return recTotalIntegers(thing);
+  }
+
+  return undefined;
 };
-  
+
+function recTotalIntegers(thing) {
+  let sum = 0;
+  if (typeof thing === "object") {
+    for (const key in thing) {
+      sum += recTotalIntegers(thing[key]);
+    }
+    //
+  } else if (typeof thing === "array") {
+    for (let i = 0; i < array.length; i++) {
+      sum += recTotalIntegers(thing[i]);
+    }
+    //
+  } else if (Number.isInteger(thing)) {
+    return 1;
+    //
+  }
+
+  return sum;
+}
+
 // Do not edit below this line
 module.exports = totalIntegers;
