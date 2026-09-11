c459634695b28880337291fd610b1558abe29726
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-10 22:35:35 -0300

    Javascript: Weather | add toggle temperature
     Small bug found when adding new temperature while in C mode.

--- JavaScript/weather_app/src/script/index.js
+++ JavaScript/weather_app/src/script/index.js
@@ -2,15 +2,42 @@ import "../css/style.css";
 import "../css/reset.css";
 
 const API_KEY = "BS237RFUVGMVKEQTF9XZYBNMU";
+const currentTime = new Date().toLocaleTimeString();
+let tempMode = "F°";
+
+// DOM stuff
 const button = document.querySelector("button");
 const form = document.querySelector("form");
-const currentTime = new Date().toLocaleTimeString();
-const city = document.querySelector("input");
+const city = document.querySelector("#input-city");
 const weatherHeader = document.querySelector("#weather-header");
 const weatherTemp = document.querySelector("#weather-temperature");
 const weatherOther = document.querySelector("#weather-other");
+const toggle = document.querySelector(".toggle-temp");
 
-console.log(`Current time: ${currentTime}`);
+toggle.addEventListener("click", () => {
+  if (tempMode === "F°") {
+    tempMode = "C°";
+  } else {
+    tempMode = "F°";
+  }
+
+  if (weatherTemp.innerHTML !== "") {
+    const newTemp = toggleTemp({
+      curTemp: Number(
+        weatherTemp.children[0].innerText.split(": ")[1].split(" ")[0],
+      ),
+      tomorrowTemp: Number(
+        weatherTemp.children[1].innerText.split(": ")[1].split(" ")[0],
+      ),
+      curSensation: Number(
+        weatherTemp.children[2].innerText.split(": ")[1].split(" ")[0],
+      ),
+    });
+
+    renderTemp(newTemp.curTemp, newTemp.tomorrowTemp, newTemp.curSensation);
+  }
+  toggle.innerText = tempMode;
+});
 
 async function getWeather(city) {
   try {
@@ -44,11 +71,13 @@ const cleanData = (obj) => {
 };
 
 function renderWeather(obj) {
+  weatherHeader.innerHTML = "";
+  weatherOther.innerHTML = "";
+
+  weatherHeader.style.opacity = "1";
+
   const h1 = document.createElement("h1");
   const des = document.createElement("p");
-  const current = document.createElement("p");
-  const feels = document.createElement("p");
-  const tomorrow = document.createElement("p");
   const humidity = document.createElement("p");
   const up = document.createElement("p");
   const down = document.createElement("p");
@@ -61,13 +90,7 @@ function renderWeather(obj) {
   weatherHeader.appendChild(des);
 
   // temperature
-  current.innerText = `Current: ${obj.curTemp} F°`;
-  tomorrow.innerText = `Tomorrow: ${obj.tomorrowTemp} F°`;
-  feels.innerText = `Feels like: ${obj.curSensation} F°`;
-  // append temperature
-  weatherTemp.appendChild(current);
-  weatherTemp.appendChild(tomorrow);
-  weatherTemp.appendChild(feels);
+  renderTemp(obj.curTemp, obj.tomorrowTemp, obj.curSensation);
 
   // other
   up.innerText = `Sun up: ${obj.sunUp}`;
@@ -76,8 +99,9 @@ function renderWeather(obj) {
   // append other
   weatherOther.appendChild(up);
   weatherOther.appendChild(down);
-  weatherOther.appendChild(feels);
+  weatherOther.appendChild(humidity);
 }
+
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
@@ -91,3 +115,47 @@ form.addEventListener("submit", async (event) => {
     throw new Error("HTTP error");
   }
 });
+
+function toggleTemp(obj) {
+  let temp = obj.curTemp;
+  let tmrTemp = obj.tomorrowTemp;
+  let feelTemp = obj.curSensation;
+
+  if (tempMode === "C°") {
+    temp = (temp - 32) / 1.8;
+    tmrTemp = (tmrTemp - 32) / 1.8;
+    feelTemp = (feelTemp - 32) / 1.8;
+
+    return {
+      curTemp: temp.toFixed(1),
+      tomorrowTemp: tmrTemp.toFixed(1),
+      curSensation: feelTemp.toFixed(1),
+    };
+  } else if (tempMode === "F°") {
+    temp = temp * 1.8 + 32;
+    tmrTemp = tmrTemp * 1.8 + 32;
+    feelTemp = feelTemp * 1.8 + 32;
+
+    return {
+      curTemp: temp.toFixed(1),
+      tomorrowTemp: tmrTemp.toFixed(1),
+      curSensation: feelTemp.toFixed(1),
+    };
+  }
+}
+
+function renderTemp(a, b, c) {
+  weatherTemp.innerHTML = "";
+
+  const current = document.createElement("p");
+  const feels = document.createElement("p");
+  const tomorrow = document.createElement("p");
+
+  current.innerText = `Current: ${a} ${tempMode}`;
+  tomorrow.innerText = `Tomorrow: ${b} ${tempMode}`;
+  feels.innerText = `Feels like: ${c} ${tempMode}`;
+
+  weatherTemp.appendChild(current);
+  weatherTemp.appendChild(tomorrow);
+  weatherTemp.appendChild(feels);
+}
