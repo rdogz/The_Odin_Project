5f56d8d120716bd190c8b2bdadc9cf94c15cab66
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-11 20:05:02 -0300

    Javascript: recursion 5 done

--- JavaScript/cs/src/5_pascal/pascal.js
+++ JavaScript/cs/src/5_pascal/pascal.js
@@ -1,6 +1,21 @@
-const pascal = function() {
-  
+const pascal = function (row) {
+  let triangle = [];
+
+  for (let col = 0; col < row; col++) {
+    triangle.push(doPascal(col, row - 1));
+  }
+
+  return triangle;
 };
-  
+
+function doPascal(col, row) {
+  // base case
+  if (col === 0 || col === row) {
+    return 1;
+  }
+
+  return doPascal(col - 1, row - 1) + doPascal(col, row - 1);
+}
+
 // Do not edit below this line
 module.exports = pascal;
