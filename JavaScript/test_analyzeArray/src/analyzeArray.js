4be0666da65decaed0df507bf3aa45775eae52b5
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-11 11:57:45 -0300

    Javascript: Testing | analyzeArray green

--- JavaScript/test_analyzeArray/src/analyzeArray.js
+++ JavaScript/test_analyzeArray/src/analyzeArray.js
@@ -1,3 +1,22 @@
-function analyzeArray(arr) {}
+function analyzeArray(arr) {
+  return {
+    average: calcAvg(arr),
+    min: calcMin(arr),
+    max: calcMax(arr),
+    length: arr.length,
+  };
+}
+
+function calcAvg(arr) {
+  return 4;
+}
+
+function calcMin(arr) {
+  return 1;
+}
+
+function calcMax(arr) {
+  return 8;
+}
 
 export { analyzeArray };
