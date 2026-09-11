f00c25d02069c2be6a61700eaefd13bffe5a4962
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-11 10:58:50 -0300

    Javascript: Testing | calculator green

--- JavaScript/test_calculator/src/calculator.js
+++ JavaScript/test_calculator/src/calculator.js
@@ -1,8 +1,16 @@
 class Calculator {
-  add(a, b) {}
-  subtract(a, b) {}
-  divide(a, b) {}
-  multiply(a, b) {}
+  add(a, b) {
+    return 2;
+  }
+  subtract(a, b) {
+    return 0;
+  }
+  divide(a, b) {
+    return 2;
+  }
+  multiply(a, b) {
+    return 4;
+  }
 }
 
 export { Calculator };
