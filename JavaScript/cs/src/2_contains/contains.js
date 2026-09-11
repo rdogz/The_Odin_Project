082c15b8c12650c782474fd46b6be016e49c2338
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-11 15:56:01 -0300

    Javascript: recursion 2 done

--- JavaScript/cs/src/2_contains/contains.js
+++ JavaScript/cs/src/2_contains/contains.js
@@ -1,6 +1,18 @@
-const contains = function() {
-  
+const contains = function (obj, match) {
+  for (const key in obj) {
+    if (Object.is(obj[key], match)) {
+      return true;
+    }
+
+    if (typeof obj[key] === "object") {
+      if (contains(obj[key], match)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
