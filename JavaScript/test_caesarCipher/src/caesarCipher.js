52992000bb1802efbb47188665746729b498c1e3
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-11 11:40:37 -0300

    Javascript: Testing | caesarCipher green&refactor

--- JavaScript/test_caesarCipher/src/caesarCipher.js
+++ JavaScript/test_caesarCipher/src/caesarCipher.js
@@ -1,5 +1,48 @@
-function caesarCipher(str) {
-  return "bcd";
+function caesarCipher(str, shift) {
+  let crypt = "";
+  shift = ((shift % 26) + 26) % 26;
+
+  let code = getUTF(str);
+  code = addNum(code, shift);
+  crypt = codeToString(code);
+
+  return crypt;
+}
+
+function getUTF(str) {
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    arr[i] = str.charCodeAt(i);
+  }
+
+  return arr;
+}
+
+function addNum(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0x41 && arr[i] <= 0x5a) {
+      if (arr[i] + num > 0x5a) {
+        arr[i] -= 26;
+      }
+      arr[i] += num;
+    } else if (arr[i] >= 0x61 && arr[i] <= 0x7a) {
+      if (arr[i] + num > 0x7a) {
+        arr[i] -= 26;
+      }
+      arr[i] += num;
+    }
+  }
+
+  return arr;
+}
+
+function codeToString(arr) {
+  let returnString = "";
+  for (let i = 0; i < arr.length; i++) {
+    returnString += String.fromCharCode(arr[i]);
+  }
+
+  return returnString;
 }
 
 export { caesarCipher };
--- JavaScript/test_caesarCipher/src/caesarCipher.test.js
+++ JavaScript/test_caesarCipher/src/caesarCipher.test.js
@@ -3,3 +3,15 @@ import { caesarCipher } from "./caesarCipher.js";
 test("shift 1 'abc' to be 'bcd'", () => {
   expect(caesarCipher("abc", 1)).toMatch("bcd");
 });
+
+test("shift 3 'xyz' to be 'abc'", () => {
+  expect(caesarCipher("xyz", 3)).toMatch("abc");
+});
+
+test("shift 3 'HeLLo' to be 'KhOOr'", () => {
+  expect(caesarCipher("HeLLo", 3)).toMatch("KhOOr");
+});
+
+test("shift 3 'Hello, World!' to be 'Khoor, Zruog!'", () => {
+  expect(caesarCipher("Hello, World!", 3)).toMatch("Khoor, Zruog!");
+});
