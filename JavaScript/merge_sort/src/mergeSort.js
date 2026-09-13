b11cc53aa17d4d42b74f72a592de7fab044a8502
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-13 18:45:39 -0300

    Javascript: mergeSort | done

--- JavaScript/merge_sort/src/mergeSort.js
+++ JavaScript/merge_sort/src/mergeSort.js
@@ -1,5 +1,44 @@
 function mergeSort(arr) {
-  return [0, 1, 1, 2, 3, 5, 8, 13];
+  if (arr.length <= 1) {
+    return arr;
+  }
+  return sort(arr);
+}
+
+function sort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  } else {
+    return merge(sort(getLeftHalf(arr)), sort(getRightHalf(arr)));
+  }
+}
+
+function merge(leftArr, rightArr) {
+  let newArr = [];
+  let index = 0;
+  let i = 0;
+  let j = 0;
+
+  while (i < leftArr.length || j < rightArr.length) {
+    if (j === rightArr.length) {
+      newArr[index++] = leftArr[i++];
+    } else if (i === leftArr.length) {
+      newArr[index++] = rightArr[j++];
+    } else if (leftArr[i] <= rightArr[j]) {
+      newArr[index++] = leftArr[i++];
+    } else {
+      newArr[index++] = rightArr[j++];
+    }
+  }
+  return newArr;
+}
+
+function getLeftHalf(arr) {
+  return arr.slice(0, Math.floor(arr.length / 2));
+}
+
+function getRightHalf(arr) {
+  return arr.slice(Math.floor(arr.length / 2), arr.length);
 }
 
 export { mergeSort };
--- JavaScript/merge_sort/src/mergeSort.test.js
+++ JavaScript/merge_sort/src/mergeSort.test.js
@@ -6,26 +6,26 @@ test("sort [3, 2, 1, 13, 8, 5, 0, 1] should equal [0, 1, 1, 2, 3, 5, 8, 13]", ()
   ]);
 });
 
-test.skip("sort [5, 4, 3, 2, 1] should equal [1, 2, 3, 4, 5]", () => {
+test("sort [5, 4, 3, 2, 1] should equal [1, 2, 3, 4, 5]", () => {
   expect(mergeSort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
 });
 
-test.skip("sort [1, 2, 3, 4, 5] should equal [1, 2, 3, 4, 5]", () => {
+test("sort [1, 2, 3, 4, 5] should equal [1, 2, 3, 4, 5]", () => {
   expect(mergeSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
 });
 
-test.skip("sort [3, 1, 2, 1, 3] should equal [1, 1, 2, 3, 3]", () => {
+test("sort [3, 1, 2, 1, 3] should equal [1, 1, 2, 3, 3]", () => {
   expect(mergeSort([3, 1, 2, 1, 3])).toEqual([1, 1, 2, 3, 3]);
 });
 
-test.skip("sort [10] should equal [10]", () => {
+test("sort [10] should equal [10]", () => {
   expect(mergeSort([10])).toEqual([10]);
 });
 
-test.skip("sort [] should equal []", () => {
+test("sort [] should equal []", () => {
   expect(mergeSort([])).toEqual([]);
 });
 
-test.skip("sort [7, 2, 9, 4, 1, 6] should equal [1, 2, 4, 6, 7, 9]", () => {
+test("sort [7, 2, 9, 4, 1, 6] should equal [1, 2, 4, 6, 7, 9]", () => {
   expect(mergeSort([7, 2, 9, 4, 1, 6])).toEqual([1, 2, 4, 6, 7, 9]);
 });
