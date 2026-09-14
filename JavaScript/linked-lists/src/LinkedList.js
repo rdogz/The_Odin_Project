ae476e470b3193b5e4f390a1acfccbace3ca6479
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-14 15:37:03 -0300

    Javascript: LinkedList | start project

---
+++ JavaScript/linked-lists/src/LinkedList.js
@@ -0,0 +1,35 @@
+import { Node } from "./Node.js";
+
+class LinkedList {
+  // adds new node containing value to the end of the list
+  append(value) {}
+
+  // adds new node containing value to the start of the list
+  prepend(value) {}
+
+  // returns total number of nodes
+  size() {}
+
+  // return the value of the first node, undefined if empty
+  head() {}
+
+  // return the value of the final node, undefined if empty
+  tail() {}
+
+  // return value of node at index
+  at(index) {}
+
+  // remove node from head and return value
+  pop() {}
+
+  // return true if value is in list
+  contains(value) {}
+
+  // return index where value is
+  findIndex(value) {}
+
+  // represents linked list objects as strings, if empty return empty string
+  toString() {}
+}
+
+export { LinkedList };
