0f1cc1f17b7170d593eafa1518fd9229ea73212a
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-15 10:55:11 -0300

    Javascript: BST | add build tree, Tree.root

--- JavaScript/binary_search_tree/src/Tree.js
+++ JavaScript/binary_search_tree/src/Tree.js
@@ -1,3 +1,5 @@
+import { mergeSort } from "./mergeSort.js";
+
 class Tree {
   constructor(arr) {
     this.root = buildTree(arr);
@@ -46,4 +48,44 @@ class Node {
 
 function buildTree(arr) {
   // sort and remove dupes
+  let sorted = mergeSort(arr);
+  sorted = clearDupes(sorted);
+
+  sorted = linkTreeNodes(sorted);
+
+  return sorted;
 }
+
+function clearDupes(arr) {
+  let retArr = [];
+  let i = 0;
+  let j = 1;
+
+  while (j < arr.length) {
+    if (arr[i] !== arr[j]) {
+      retArr.push(arr[i]);
+    }
+    i++;
+    j++;
+  }
+
+  retArr.push(arr[i]);
+  return retArr;
+}
+
+function linkTreeNodes(arr) {
+  if (arr.length === 0) return null;
+  const halfArr = Math.floor(arr.length / 2);
+  const root = new Node(arr[halfArr]);
+
+  if (arr.length === 1) {
+    return root;
+  } else {
+    root.left = linkTreeNodes(arr.slice(0, halfArr));
+    root.right = linkTreeNodes(arr.slice(halfArr + 1, arr.length));
+  }
+
+  return root;
+}
+
+export { Tree };
