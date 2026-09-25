827d4ac87d31f4525f63ac0f31bc314120bd4ba8
rdogz <213889694+rdogz@users.noreply.github.com>
2026-09-25 00:05:13 -0300

    Advanced: Popup exercise done

--- Advanced/animation/02-pop-up/script.js
+++ Advanced/animation/02-pop-up/script.js
@@ -1,12 +1,24 @@
-const openButton = document.getElementById('trigger-modal');
-const closeButton = document.getElementById('close-modal');
+const openButton = document.getElementById("trigger-modal");
+const closeButton = document.getElementById("close-modal");
 
 function toggleModal() {
-  const modalDiv = document.querySelector('.popup-modal');
-  const backdrop = document.querySelector('.backdrop');
-  modalDiv.classList.toggle('show');
-  backdrop.classList.toggle('show');
+  const modalDiv = document.querySelector(".popup-modal");
+  const backdrop = document.querySelector(".backdrop");
+  modalDiv.classList.remove("hide");
+  backdrop.classList.remove("hide");
+  modalDiv.classList.toggle("show");
+  backdrop.classList.toggle("show");
 }
 
-openButton.addEventListener('click', toggleModal);
-closeButton.addEventListener('click', toggleModal);
+function closeModal() {
+  const modalDiv = document.querySelector(".popup-modal");
+  const backdrop = document.querySelector(".backdrop");
+  modalDiv.classList.remove("show");
+  backdrop.classList.remove("show");
+  modalDiv.classList.toggle("hide");
+  backdrop.classList.toggle("hide");
+}
+
+openButton.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", closeModal);
+
